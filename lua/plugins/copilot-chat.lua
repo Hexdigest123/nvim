d04db7de0afa4adf7905f8be10e75d350e296c8e
Hexdigest123 <pierre.maurice.m256@googlemail.com>
2024-06-13 02:28:52 +0200

    removed code, removed remap

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,60 +1,3 @@
-function IS_GIT_REPO()
-  local f = io.popen("git rev-parse --is-inside-work-tree 2>/dev/null")
-  if not f then
-    return false
-  end
-  local git_repo = f:read("*all")
-  f:close()
-  if git_repo:match("true") then
-    return true
-  else
-    return false
-  end
-end
-
-function GET_CONTEXT()
-  local depth = vim.env.NVDEPTH or 4
-  if not IS_GIT_REPO() then
-    return false
-  end
-
-  local lines = {}
-
-  local p = io.popen("git rev-parse --show-toplevel")
-  if not p then
-    return false
-  end
-  local git_root = p:read("*all"):gsub("\n", "")
-  p:close()
-  local cmd = "find " .. git_root .. " -maxdepth " .. depth .. " -type f"
-  local files = io.popen(cmd):lines()
-
-  for file in files do
-    if file:match(".git") then
-      goto continue
-    end
-    if file:match("node_modules") then
-      goto continue
-    end
-    local f = io.open(file, "r")
-    if not f then
-      return false
-    end
-    local content = f:read("*all"):gsub("\n", "")
-    if content ~= nil then
-      f:close()
-      print(content)
-      table.insert(lines, file .. " has the content: " .. content)
-    end
-    ::continue::
-  end
-  local line_string = ""
-  for line in lines do
-    line_string = line_string .. line .. "\n"
-  end
-  return line_string
-end
-
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   branch = "canary",
@@ -111,7 +54,6 @@ return {
   config = function(_, opts)
     local chat = require("CopilotChat")
     require("CopilotChat.integrations.cmp").setup()
-
     vim.api.nvim_create_autocmd("BufEnter", {
       pattern = "copilot-chat",
       callback = function()
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,12 +1,5 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
-        ["<C-CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<CR>"] = cmp.config.disable,
-      })
-    end,
   },
 }
