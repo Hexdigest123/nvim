68c192991c7ad9be1c799cbd6c6b02a397597b14
Pierre-Maurice Merckel <pierre@merckel.dev>
2024-08-24 16:12:00 +0200

    Updated Keybinds for vim-fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,6 +1,9 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.keymap.set("n", "<leader>G", ":Git<CR>")
+    vim.keymap.set("n", "<leader>GG", ":Git<CR>")
+    vim.keymap.set("n", "<leader>GC", ":Git commit<CR>")
+    vim.keymap.set("n", "<leader>GP", ":Git push<CR>")
+    vim.keymap.set("n", "<leader>Gp", ":Git pull<CR>")
   end,
 }
