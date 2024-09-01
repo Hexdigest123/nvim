9855503018ca1b35cfa37d3eb326e871c7991332
Pierre-Maurice Merckel <pierre@merckel.dev>
2024-09-01 05:12:02 +0200

    added js/ts formatter and linter support

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,10 +1,16 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = { "nvimtools/none-ls-extras.nvim" },
   config = function()
     local nls = require("null-ls")
     local sources = {
+      -- Python formatting and linting
       nls.builtins.formatting.black,
       nls.builtins.diagnostics.pylint,
+
+      -- JS/TS formatting and linting
+      nls.builtins.formatting.prettierd,
+      require("none-ls.diagnostics.eslint_d"),
     }
 
     nls.setup({
