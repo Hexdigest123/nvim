5c2eb181a1e18e95c799ff5b2a2f69b696d4e7d2
Hexdigest123 <pierre@merckel.dev>
2026-01-06 18:10:04 +0100

    ADD/FIX: opencode and treesitter insufficient for latex

--- lua/hexdigest/plugins/treesitter.lua
+++ lua/hexdigest/plugins/treesitter.lua
@@ -30,6 +30,7 @@ return {
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
+        disable = { "latex" },
       },
       indent = {
         enable = true,
