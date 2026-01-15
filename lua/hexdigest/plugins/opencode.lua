89e31c831eef1acdac8f3cab7c260bd4286e1d0f
Hexdigest123 <pierre@merckel.dev>
2026-01-15 20:46:25 +0100

    CHG: formatter

--- lua/hexdigest/plugins/opencode.lua
+++ lua/hexdigest/plugins/opencode.lua
@@ -39,7 +39,6 @@ return {
     vim.keymap.set("n", "<S-C-d>", function()
       require("opencode").command("session.half.page.down")
     end, { desc = "opencode half page down" })
-
     -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
     vim.keymap.set("n", "+", "<C-a>", { desc = "Increment", noremap = true })
     vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
