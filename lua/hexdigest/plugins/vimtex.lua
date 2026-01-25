fefb68fa4f12b4077c86315a4866408e092bc684
Hexdigest123 <pierre@merckel.dev>
2026-01-25 20:28:45 +0100

    CHG: zathura too skim

--- lua/hexdigest/plugins/vimtex.lua
+++ lua/hexdigest/plugins/vimtex.lua
@@ -1,14 +1,14 @@
 return {
   "lervag/vimtex",
   init = function()
-    vim.g["vimtex_view_method"] = "zathura"
+    vim.g["vimtex_view_method"] = "skim"
     vim.g["vimtex_quickfix_mode"] = 0
     vim.g["vimtex_mappings_enabled"] = 1
     vim.g["vimtex_indent_enabled"] = 0
     vim.g["tex_flavor"] = "latex"
     vim.g["tex_indent_items"] = 0
     vim.g["tex_indent_brace"] = 0
-    vim.g["vimtex_context_pdf_viewer"] = "zathura"
+    vim.g["vimtex_context_pdf_viewer"] = "skim"
     vim.g["vimtex_compiler_latexmk"] = {
       aux_dir = "/tmp/vimtex/",
       out_dir = "./out",
