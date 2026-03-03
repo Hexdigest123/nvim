47588677fec75919a7d5b2d1ad0568ce0f34df7a
Hexdigest123 <pierre@merckel.dev>
2026-03-03 21:37:17 +0100

    fix(ui): transparency issues

--- lua/hexdigest/plugins/oil.lua
+++ lua/hexdigest/plugins/oil.lua
@@ -1,9 +1,17 @@
 return {
   "stevearc/oil.nvim",
   init = function()
-    require("oil").setup()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+      float = {
+        win_options = {
+          winblend = 0,
+        },
+      },
+    })
   end,
-  opts = {},
   keys = {
     {
       "<leader>e",
